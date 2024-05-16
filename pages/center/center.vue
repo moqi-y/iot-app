189cad4af1ec16675b3320c269c3ae7b13f52334
yuanzhen <yuanzhenwork@qq.com>
2024-05-16 23:15:47 +0800

    style: 调整样式

--- pages/center/center.vue
+++ pages/center/center.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-
+		<view class="nav-bar">个人中心</view>
 		<view class="head">
 			<view class="info">
 				<view class="user">
@@ -10,7 +10,7 @@
 				<view class="btn">
 					<uni-icons type="notification" size="28" color="#1296db"></uni-icons>
 					消息中心
-					</view>
+				</view>
 			</view>
 		</view>
 		<view class="menu-box">
@@ -69,135 +69,150 @@
 	</view>
 </template>
 <script>
-	export default {
-		data() {
-			return {
-
-			}
-		},
-		methods: {
+export default {
+	data() {
+		return {
 
 		}
+	},
+	methods: {
+
 	}
+}
 </script>
 
 <style lang="scss" scoped>
-	page {
-		background-color: #f8f8f8;
-	}
+page {
+	background-color: #f8f8f8;
+}
 
-	.head {
-		position: relative;
-		width: 100%;
-		height: 400rpx;
-		background-color: #ff7962;
-		background-image: url("/static/images/bird.jpg");
-		background-size: cover;
-	}
+.nav-bar {
+	width: 70px;
+	text-align: center;
+	position: absolute;
+	z-index: 9999;
+	font-size: 16px;
+	color: #fff;
+	left: 152.5px;
+	top: 20px;
+	font-weight: 600;
+}
 
-	.head .info {
-		padding-top: 150rpx;
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
+.head {
+	position: relative;
+	width: 100%;
+	height: 400rpx;
+	background-color: #ff7962;
+	background-image: url("/static/images/bird.jpg");
+	background-size: cover;
+}
 
-	.head .info .user {
-		padding-left: 50rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+.head .info {
+	padding-top: 150rpx;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
 
-	.head .info .user image {
-		width: 120rpx;
-		height: 120rpx;
-	}
+.head .info .user {
+	padding-left: 50rpx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	.head .info .user .name {
-		padding-left: 20rpx;
-		font-size: 28rpx;
-		font-weight: 600;
-		color: #FFFFFF;
-		display: flex;
-		justify-content: center;
-		align-items: flex-start;
-		flex-direction: column;
-	}
+.head .info .user image {
+	width: 120rpx;
+	height: 120rpx;
+}
 
-	.head .info .user .name text {
-		line-height: 40rpx;
-	}
+.head .info .user .name {
+	padding-left: 20rpx;
+	font-size: 28rpx;
+	font-weight: 600;
+	color: #FFFFFF;
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	flex-direction: column;
+}
 
-	.head .info .btn {
-		text-align: center;
-		background-color: #ff907c;
-		color: #FFFFFF;
-		font-size: 30rpx;
-		width: 200rpx;
-		height: 80rpx;
-		line-height: 80rpx;
-		border-top-left-radius: 10rpx;
-		border-bottom-left-radius: 10rpx;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
+.head .info .user .name text {
+	line-height: 40rpx;
+}
 
-	.menu-box {
-		position: absolute;
-		top: 300rpx;
-		width: 100%;
-		margin-bottom: 120rpx;
-	}
+.head .info .btn {
+	text-align: center;
+	background-color: #ff907c;
+	color: #FFFFFF;
+	font-size: 30rpx;
+	width: 200rpx;
+	height: 80rpx;
+	line-height: 80rpx;
+	border-top-left-radius: 10rpx;
+	border-bottom-left-radius: 10rpx;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
 
-	.region {
-		background-color: #FFFFFF;
-		margin: 30rpx;
-		border-radius: 10rpx;
-	}
+.menu-box {
+	position: absolute;
+	top: 300rpx;
+	width: 100%;
+	margin-bottom: 120rpx;
+}
 
-	.region .title {
-		font-size: 32rpx;
-		font-weight: bold;
-		padding: 30rpx;
-	}
+.region {
+	background-color: #FFFFFF;
+	margin: 30rpx;
+	border-radius: 10rpx;
+}
 
-	.region .menu-row {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+.region .title {
+	font-size: 32rpx;
+	font-weight: bold;
+	padding: 30rpx;
+}
 
-	.region .menu-row .cell {
-		width: 30%;
-		height: 200rpx;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-	}
+.region .menu-row {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	.region .menu-row .cell .count {
-		font-size: 40rpx;
-	}
+.region .menu-row .cell {
+	width: 30%;
+	height: 200rpx;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
 
-	.region .menu-row .cell .name {
-		font-size: 30rpx;
-		line-height: 100rpx;
-		color: #8a8a8a;
-	}
-	.online{
-		color: #1ec33ac1;
-		font-weight: bold;
-	}
-	.offline{
-		color: red;
-		font-weight: bold;
-	}
-	.warning{
-		color: #f08008;
-		font-weight: bold;
-	}
+.region .menu-row .cell .count {
+	font-size: 40rpx;
+}
+
+.region .menu-row .cell .name {
+	font-size: 30rpx;
+	line-height: 100rpx;
+	color: #8a8a8a;
+}
+
+.online {
+	color: #1ec33ac1;
+	font-weight: bold;
+}
+
+.offline {
+	color: red;
+	font-weight: bold;
+}
+
+.warning {
+	color: #f08008;
+	font-weight: bold;
+}
 </style>
--- pages/device/device.vue
+++ pages/device/device.vue
@@ -15,7 +15,8 @@
 		<view class="device-info">
 			<view class="device-details" v-for="(item, index) in deviceImages" :key="index">
 				<image class="device-image" :mode="item.mode" :src="item.url" @error="imageError"></image>
-				<view class="device-name">{{ item.name }}<view class="dot"></view></view>
+				<view class="device-name">{{ item.name }}<view class="dot"></view>
+				</view>
 			</view>
 		</view>
 
@@ -73,17 +74,13 @@ const bottomInfo = ref(['通用场景']);
 	width: 100%;
 	position: fixed;
 	top: 0px;
+	z-index: 99999;
 }
 
-.time {
-	font-size: 18px;
-	font-weight: bold;
-	color: #fff;
-}
 
 .nav-items {
 	display: flex;
-	margin:0 10px;
+	margin: 0 10px;
 }
 
 .scan-area {
@@ -109,7 +106,11 @@ const bottomInfo = ref(['通用场景']);
 	margin-right: 20px;
 	font-size: 16px;
 	color: #fff;
+	font-weight: 600;
 }
+
+/* 元素最后一个 */
+
 
 .device-info {
 	width: 100%;
@@ -143,34 +144,40 @@ const bottomInfo = ref(['通用场景']);
 	margin-left: 4px;
 }
 
-.dot{
+.dot {
 	width: 12px;
 	height: 12px;
 	background-color: #1ec33ac1;
-	border-color:#1ec33ac1;
+	border-color: #1ec33ac1;
 	border-radius: 50%;
 	margin-left: 4px;
 }
 
 .menu-list {
-	width: 100%;
-	margin-top: 20px;
-	display: flex;
-	flex-wrap: wrap;
+	display: grid;
+  grid-template-columns: repeat(2, 1fr); /* 创建两列，每列等宽 */
+  gap: 5px; /* 设置网格项目之间的竖向间距为20px */
+  grid-auto-rows: minmax(0, auto); /* 设置网格行的最小高度和最大高度 */
+  padding: 10px; /* 设置容器的内边距为10px */
+  box-sizing: border-box; /* 确保内边距不会增加容器的总宽度 */
+  justify-content: space-between; /* 网格行之间的间距自动平分 */
 }
 
 .menu-item {
 	display: flex;
+	/* 使用弹性盒子布局 */
 	justify-content: center;
+	/* 水平居中 */
 	align-items: center;
+	/* 垂直居中 */
 	padding: 15px 0;
 	background: linear-gradient(45deg, #45e0f0, #679ef0);
 	color: #fff;
 	border-radius: 15px;
 	margin-bottom: 10px;
 	box-shadow: 0 2px 10px rgba(101, 158, 240, 0.3);
-	width: 40%;
-	height: 60px;
+	width: 170px;
+	height: 50px;
 	margin: 12px;
 }
 
