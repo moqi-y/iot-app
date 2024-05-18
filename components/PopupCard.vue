5358c6e0f671841fa59fd753c463b07891eb1e31
yuanzhen <yuanzhenwork@qq.com>
2024-05-18 11:48:24 +0800

    feat: 新增快速添加设备弹窗

---
+++ components/PopupCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <view class="popup-card">
+        <view class="card-icon">
+            <image class="card-icon-img" :src="menuItem.icon"></image>
+        </view>
+        <view class="card-name">
+            {{ menuItem.name }}
+        </view>
+    </view>
+</template>
+
+<script setup>
+const props = defineProps({
+    menuItem:{}
+})
+</script>
+
+<style scoped>
+.popup-card {
+    width: 95%;
+    height: 100px;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+.card-icon{
+    width: 35%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card-name{
+    width: 60%;
+    font-size: 22px;
+    font-weight: 600;
+    color: #333333;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.card-icon-img {
+    width: 70px;
+    height: 70px;
+}
+</style>
